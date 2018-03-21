48802b23b3aefe4edd4e7f5847ac618829b0a6e3
Heidi Hlavinka <hchlav@gmail.com>
2018-03-21 00:48:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,9 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(cheddar_cheese)
+  cheese_types = ["cheddar", "gouda", "camembert"]
   cheddar_cheese.tap do |cheesus|
-    
+    cheese_string = cheesus & 
 
 # def find_the_cheese(cheddar_cheese)
 #   cheese_types = ["cheddar", "gouda", "camembert"]
